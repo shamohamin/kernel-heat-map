857c860451c03ac1717916cc58491319abcdc33a
armin-afsharian <armin8545@gmail.com>
2023-11-23 17:45:01 -0500

    Add report functionality

--- process_time/terminal.py
+++ process_time/terminal.py
@@ -3,18 +3,20 @@ import subprocess
 import signal
 import os
 
-RECORD_CMD = 'record'
 
 class Terminal:
-    def __init__(self, trace_cmd) -> None:
-        self.trace_cmd: TraceCmd = trace_cmd
+    def __init__(self, trace_cmd: TraceCmd) -> None:
+        self.trace_cmd = trace_cmd
 
     def run(self):
         self.record()
+        self.report()
 
-    def record(self):
+    def record(self) -> None:
         cmd = self.trace_cmd.get_cmd(RECORD_CMD)
         subprocess.run(cmd)
         
-    def report():
-        pass
+    def report(self) -> None:
+        cmd = self.trace_cmd.get_cmd(REPORT_CMD)
+        subprocess.run(cmd, shell=True)
+        
--- process_time/trace_cmd.py
+++ process_time/trace_cmd.py
@@ -1,23 +1,27 @@
+RECORD_CMD = 'record'
+REPORT_CMD = 'report'
+
 class TraceCmd():
     def __init__(self, pid=None) -> None:
         self.plugin = "function_graph"
         self.pid = pid
         self.report_file = "trace.report"
     
-    def get_cmd(self, type):
-        if(type == 'report'):
+    def get_cmd(self, type: str) -> list:
+        if(type == REPORT_CMD):
             return self.build_report_cmd()
-        elif(type == 'record'):
+        elif(type == RECORD_CMD):
             return self.build_record_cmd()
     
-    def build_record_cmd(self) -> str:
+    def build_record_cmd(self) -> list:
         cmd = ['sudo', 'trace-cmd', 'record', "-p {} ".format(self.plugin)]
         if(self.pid != None):
             cmd.append("-P {} ".format(self.pid))
         cmd.append('sleep')
-        cmd.append('1')
+        cmd.append('0.1')
         return cmd
 
-    def build_report_cmd(self) -> str:
+    def build_report_cmd(self) -> list:
+        #cmd = ['trace-cmd', 'report', '>', ]
         cmd = "trace-cmd report > {}".format(self.report_file)
         return cmd
